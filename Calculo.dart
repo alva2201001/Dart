2e6ac9e2ab179cf606faad5d0b747035069c4085
ALVARO VENCIGUERRA JUNIOR <alvaro.3900@aluno.pr.senac.br>
2023-04-11 14:26:54 -0300

    first commit

---
+++ Calculo.dart
@@ -0,0 +1,10 @@
+void main() {
+ temp(19);
+}
+
+void temp (double c){
+  double f = (9 * c + 160) / 5;
+  print(f);
+}
+ 
+
--- iti.dart
+++ iti.dart
@@ -2,10 +2,16 @@ void main() {
  Robs ("Veigar", "Nautilos");
  Robs ("Veigar", "Diana");
  Robs ("Veigar", "Wukong");
- Robs ("Veigar" , "Pyke");
+ Robs ("Veigar" , "Pyke", false);
 }
 
-void Robs(String lol, games) {
+void Robs(String lol, games, [bool dia = true]) {
   print("O boneco $lol é o melhor boneco");
     print("O champion $games é o melhor butequinho");
+
+    if (dia == true) {
+      print("Bom buneco");
+    } else{
+      print("Bom champion");
+    }
 }
